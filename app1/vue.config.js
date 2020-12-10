638963988e5f81ae7751a03eda80c5ecfedaf4e7
Joel Denning <joeldenning@gmail.com>
2020-12-10 13:21:47 -0700

    Show workaround

--- app1/vue.config.js
+++ app1/vue.config.js
@@ -3,5 +3,13 @@ module.exports = {
     externals: {
       vue: 'Vue'
     }
+  },
+  chainWebpack(config) {
+    // UNCOMMENT THIS to work around the bug
+
+    // config.module.rule('vue').use('vue-loader').tap(options => {
+    //   options.hotReload = false;
+    //   return options
+    // })
   }
 }
